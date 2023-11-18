2226be20f241c74a5c2a41d231d811d066821563
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-11-18 14:49:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 修改默认IP
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
+# 添加luci-app-mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
