a96c4d8505a66914c0d2e052e8265c08a63ae2b8
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-11-21 15:47:24 +0800

    Update diy-part1.sh

--- Scripts/diy-part1.sh
+++ Scripts/diy-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld;main' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 #grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
