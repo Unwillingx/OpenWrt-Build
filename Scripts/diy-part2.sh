49cac860ab36cef377e4102a7bcc1b5afa75eed4
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-12-15 15:51:00 +0800

    Update diy-part2.sh

--- Scripts/diy-part2.sh
+++ Scripts/diy-part2.sh
@@ -2,6 +2,7 @@
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
+sed -i "s/local navbar_proxy = 'shadowsocksr'/local navbar_proxy = 'openclash'/g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
 # 添加luci-app-mosdns
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
