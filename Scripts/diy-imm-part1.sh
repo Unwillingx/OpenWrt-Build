dc87e7502be934d51f1fb9b09369f75deb93e683
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-12-17 16:29:33 +0800

    Update diy-imm-part1.sh

--- Scripts/diy-imm-part1.sh
+++ Scripts/diy-imm-part1.sh
@@ -2,5 +2,4 @@
 # Add a feed source
 echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 #grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
