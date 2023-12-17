f561f4a0627f89ac2fa28ffde3d1ecdb83148b4a
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-12-17 17:03:43 +0800

    Update diy-imm-part2.sh

--- Scripts/diy-imm-part2.sh
+++ Scripts/diy-imm-part2.sh
@@ -3,9 +3,9 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
 # 添加luci-app-mosdns
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
+rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# 添加luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
