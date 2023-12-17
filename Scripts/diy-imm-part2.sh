b15e18fc007c0cc544971c2b48087bfb832af9f1
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-12-17 16:30:01 +0800

    Update diy-imm-part2.sh

--- Scripts/diy-imm-part2.sh
+++ Scripts/diy-imm-part2.sh
@@ -7,6 +7,7 @@ find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone -b master --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
 ./scripts/feeds update -a
 ./scripts/feeds install -a
