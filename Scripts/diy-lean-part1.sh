867dccaa5bd9d3df7a64aab281422d642b7a6568
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-12-17 21:02:10 +0800

    Update diy-lean-part1.sh

--- Scripts/diy-lean-part1.sh
+++ Scripts/diy-lean-part1.sh
@@ -5,7 +5,4 @@ echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> f
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-# 替换luci-app-watchcat
-rm -rf feeds/luci/applications/luci-app-watchcat
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-watchcat package/luci-app-watchcat
 #grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
