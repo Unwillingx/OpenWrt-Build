58e8ab8cce40a67103d31d6090eb9aa0ab1641f0
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-12-17 17:12:47 +0800

    Update diy-sdf-part1.sh

--- Scripts/diy-sdf-part1.sh
+++ Scripts/diy-sdf-part1.sh
@@ -5,6 +5,11 @@ sed -i '$d' feeds.conf.default
 echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 ./scripts/feeds update -a
+./scripts/feeds install -a
+# 替换luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
-git clone -b master --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
+svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+# 替换luci-app-watchcat
+rm -rf feeds/luci/applications/luci-app-watchcat
+svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-watchcat package/luci-app-watchcat
 #grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
