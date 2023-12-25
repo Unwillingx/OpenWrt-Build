9bd0daa0c93f2fdb0203f845d6a27853fdbafe0f
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-12-25 11:02:44 +0800

    Update diy-imm-part1.sh

--- Scripts/diy-imm-part1.sh
+++ Scripts/diy-imm-part1.sh
@@ -2,6 +2,9 @@
 # Add a feed source
 echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 #grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
