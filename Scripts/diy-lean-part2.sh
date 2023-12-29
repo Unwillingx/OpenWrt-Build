faf94a0eb7f0ca5f187aefc26ab883b61da96efd
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2023-12-30 02:34:11 +0800

    Update diy-lean-part2.sh

--- Scripts/diy-lean-part2.sh
+++ Scripts/diy-lean-part2.sh
@@ -9,4 +9,6 @@ find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # 添加luci-app-alist
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-alist package/alist
