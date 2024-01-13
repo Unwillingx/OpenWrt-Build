af6e0c8d1b66d8069ca2d8fd99277292f65ac925
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-01-13 22:56:13 +0800

    Create diy-sdf-part2.sh

---
+++ Scripts/diy-sdf-part2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
+# sed -i "s/local navbar_proxy = 'shadowsocksr'/local navbar_proxy = 'openclash'/g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
+# 添加luci-app-mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# 添加luci-app-openclash
+sudo rm -rf /feeds/luci/applications/luci-app-openclash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git -b master package/luci-app-openclash
+# 修改python
+sudo mkdir python
+sudo chmod -R 777 python
+git clone https://github.com/openwrt/packages.git python && cd python && git checkout 246f361b06a9c99cd21c0bc06fa5141198916054
+cd ..
+sudo rm -rf feeds/packages/lang/python
+cp -rf python/lang/python feeds/packages/lang/python
+sudo rm -rf python
