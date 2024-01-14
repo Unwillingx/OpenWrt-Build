028321e6a00e867e53b1a96ec2529cda172e8e7d
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-01-14 11:34:22 +0800

    Update diy-ecm-part2.sh

--- Scripts/diy-ecm-part2.sh
+++ Scripts/diy-ecm-part2.sh
@@ -3,6 +3,8 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
 # 添加luci-app-mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
