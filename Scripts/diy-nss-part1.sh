d08fe1d6e093f8a94b0632e7757e40dec65c3645
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-01-19 20:14:02 +0800

    Update diy-nss-part1.sh

--- Scripts/diy-nss-part1.sh
+++ Scripts/diy-nss-part1.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Add a feed source
-sed -i '$d' feeds.conf.default
-sed -i '$d' feeds.conf.default
-sed -i '$d' feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
 echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
