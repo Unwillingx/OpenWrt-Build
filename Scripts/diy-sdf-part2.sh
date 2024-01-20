5cb9eb84b61ab004dcd3e68511328b34155c2771
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-01-20 15:55:53 +0800

    Update diy-sdf-part2.sh

--- Scripts/diy-sdf-part2.sh
+++ Scripts/diy-sdf-part2.sh
@@ -19,3 +19,10 @@ git clone --depth=1 https://github.com/openwrt/packages.git -b master watchcat
 cp -rf watchcat/utils/watchcat feeds/packages/utils/watchcat
 sudo rm -rf watchcat
 git clone --depth=1 https://github.com/gngpp/luci-app-watchcat-plus.git -b main package/luci-app-watchcat-plus
+# 更换python
+sudo rm -rf feeds/packages/lang/python
+sudo mkdir python
+sudo chmod -R 777 python
+git clone --depth=1 https://github.com/coolsnowwolf/packages.git -b openwrt-19.07 python
+cp -rf python/lang/python feeds/packages/lang/python
+sudo rm -rf python
