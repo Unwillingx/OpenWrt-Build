64980cc7a00ee9c738e8aaa9e70c6997f0f36743
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-01-20 16:12:20 +0800

    Update diy-ecm-part1.sh

--- Scripts/diy-ecm-part1.sh
+++ Scripts/diy-ecm-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Add a feed source
-echo 'src-git ssrp https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
+# echo 'src-git ssrp https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 #grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
