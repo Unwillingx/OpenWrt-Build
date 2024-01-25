1a812c83f91342879a252b5a966a0a73c42c4f31
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-01-25 23:30:12 +0800

    Delete Scripts/diy-lean-part1.sh

--- Scripts/diy-lean-part1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Add a feed source
-echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-./scripts/feeds update -a
-./scripts/feeds install -a
-#grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
