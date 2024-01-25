36e034d252cae9e3c97ee0655869c0c331452625
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-01-25 23:31:23 +0800

    Delete Scripts/diy-nss-part2.sh

--- Scripts/diy-nss-part2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
-# 添加luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git clone -b js https://github.com/gngpp/luci-theme-design.git package/luci-theme-design
