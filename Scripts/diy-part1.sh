da29bac2ae1ae043095ec6a0ab10ccd243cd9607
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-01-25 23:31:36 +0800

    Delete Scripts/diy-part1.sh

--- Scripts/diy-part1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld;main' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-#grep -wblr firewall ./include/target.mk | xargs sed -i 's/\<firewall\>/firewall4/g'
