2dc171bfcc728e7f3ad7cb3e0a512b3cc9ef5a2b
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-01-26 16:48:50 +0800

    Update preset-openclash.sh

--- Scripts/preset-openclash.sh
+++ Scripts/preset-openclash.sh
@@ -3,7 +3,7 @@
 mkdir -p files/etc/openclash/core
 mkdir -p files/etc/config
 
-CLASH_META_URL="https://github.com/vernesong/OpenClash/blob/core/master/meta/clash-linux-${1}.tar.gz"
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 CONFIG_URL="https://raw.githubusercontent.com/Unwillingx/OpenWrt-Build/main/Configs/openclash.config"
