0d5a66b13d0e8a7b257ffdb0f25bc38347aebb86
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-02-19 15:44:43 +0800

    Update diy-mt3000-imm-part2.sh

--- Scripts/diy-mt3000-imm-part2.sh
+++ Scripts/diy-mt3000-imm-part2.sh
@@ -9,10 +9,10 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git -b master package/luci-app-openclash
 # 修改python
-sudo mkdir python
-sudo chmod -R 777 python
-git clone https://github.com/immortalwrt/packages.git python && cd python && git checkout c2904e115185493d3429e7e7c15e0752ce76a35f
-cd ..
-sudo rm -rf feeds/packages/lang/python
-cp -rf python/lang/python feeds/packages/lang/python
-sudo rm -rf python
+# sudo mkdir python
+# sudo chmod -R 777 python
+# git clone https://github.com/immortalwrt/packages.git python && cd python && git checkout c2904e115185493d3429e7e7c15e0752ce76a35f
+# cd ..
+# sudo rm -rf feeds/packages/lang/python
+# cp -rf python/lang/python feeds/packages/lang/python
+# sudo rm -rf python
