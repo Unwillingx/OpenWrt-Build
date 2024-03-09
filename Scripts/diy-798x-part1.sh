9c28833ebb36b9dbb3fee2a28fbfd82ca5e84884
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-03-09 15:29:38 +0800

    Update diy-798x-part1.sh

--- Scripts/diy-798x-part1.sh
+++ Scripts/diy-798x-part1.sh
@@ -2,5 +2,6 @@
 # Add a feed source
 # echo 'src-git passwall2_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >> feeds.conf.default
+git checkout 24546fd666040cc3e6d894e42bbeffc87d487ee7
 ./scripts/feeds update -a
 ./scripts/feeds install -a
