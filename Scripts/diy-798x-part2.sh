ca6d91823e95fef10a303146f9bd0022f38fa9cd
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-03-15 00:38:41 +0800

    Update diy-798x-part2.sh

--- Scripts/diy-798x-part2.sh
+++ Scripts/diy-798x-part2.sh
@@ -3,7 +3,7 @@
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
 # 添加luci-app-mosdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
