d5983970879d156a8d1159ac3b4222a2f63081d6
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-03-19 23:55:07 +0800

    Update diy-798x-part2.sh

--- Scripts/diy-798x-part2.sh
+++ Scripts/diy-798x-part2.sh
@@ -2,11 +2,14 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
 # 添加luci-app-mosdns
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
-# rm -rf feeds/packages/net/v2ray-geodata
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+cd package/mosdns && git checkout 169ae0510ce5acde11898a656b09c04883d31986
+cd ..
+cd ..
 # 添加luci-app-ssrplus
 # rm -rf feeds/luci/applications/luci-app-ssr-plus
 # git clone --depth=1 https://github.com/fw876/helloworld.git -b master package/helloworld
