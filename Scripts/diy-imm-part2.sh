b4451b2c6d6bc629cf1556db38b04cae8a9c3c4d
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-03-21 17:43:17 +0800

    Update diy-imm-part2.sh

--- Scripts/diy-imm-part2.sh
+++ Scripts/diy-imm-part2.sh
@@ -3,6 +3,9 @@
 sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
 # 添加luci-app-mosdns
 rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # 添加luci-app-openclash
