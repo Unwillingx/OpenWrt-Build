fce4f8d2c4ccfca22d9a80b8e344fdd19d8944c6
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-03-23 17:37:07 +0800

    Update diy-798x-part2.sh

--- Scripts/diy-798x-part2.sh
+++ Scripts/diy-798x-part2.sh
@@ -7,9 +7,6 @@ git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/l
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-cd package/mosdns && git checkout 169ae0510ce5acde11898a656b09c04883d31986
-cd ..
-cd ..
 # 添加luci-app-ssrplus
 # rm -rf feeds/luci/applications/luci-app-ssr-plus
 # git clone --depth=1 https://github.com/fw876/helloworld.git -b master package/helloworld
