635f6cc831a3344601ebf087d829458d5b7d80b1
Unwillingx <125542178+Unwillingx@users.noreply.github.com>
2024-03-28 03:03:02 +0800

    Update preset-openclash.sh

--- Scripts/preset-openclash.sh
+++ Scripts/preset-openclash.sh
@@ -13,14 +13,17 @@ CONFIG_URL="https://raw.githubusercontent.com/Unwillingx/OpenWrt-Build/main/Conf
 release_info=$(wget -qO- https://api.github.com/repos/MetaCubeX/mihomo/releases/latest)
 # 解析JSON以获取最新发布的tag
 tag=$(echo "$release_info" | grep -oP '"tag_name": "\K(.*?)(?=")')
+echo "$tag"
 # 构建文件名
 file_name="mihomo-linux-arm64-$tag.gz"
+echo "$file_name"
 # 解析JSON以获取文件下载链接
 download_url=$(echo "$release_info" | grep -oP '"browser_download_url": "\K(.*?)(?=")' | grep "$file_name")
+echo "$download_url"
 # 下载文件
 if [ -n "$download_url" ]; then
     echo "Downloading latest release from: $download_url"
-    wget -qO- "$download_url" | zcat xOvz > files/etc/openclash/core/clash_meta
+    wget -qO- "$download_url" | gunzip -c > files/etc/openclash/core/clash_meta
     echo "Download complete."
 else
     echo "Failed to retrieve download URL for $file_name."
